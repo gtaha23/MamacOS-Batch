dd4a40a4683c6bc13348bc29255f8bd2f762b019
gtaha230812 <159329966+gtaha23@users.noreply.github.com>
2026-04-16 14:40:35 +0300

    Fixed privacy issues on updater

--- system/updater.bat
+++ system/updater.bat
@@ -9,6 +9,7 @@ echo Downloading the last version,
 echo Downloading the menu. . .
 goto :Download
 
+:: Change the download location for the command to work!
 :Download
 echo downloading. . .
 ping localhost -n 2 > nul
@@ -16,8 +17,8 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/gtaha23/MamacOS-Batch/blob/main/Menu.bat', 'Menu.bat')"
 echo moving the files. . .
 ping localhost -n 3 > nul
-move "C:\Users\Gta23\OneDrive\Masaüstü\MamacOS\system\Ders.bat" - "C:\Users\Gta23\OneDrive\Masaüstü\MamacOS"
-move "C:\Users\Gta23\OneDrive\Masaüstü\MamacOS\system\Menu.bat" - "C:\Users\Gta23\OneDrive\Masaüstü\MamacOS"
+move "C:\Users\*username*\*Saved Place*\MamacOS\system\Ders.bat" - "C:\Users\*username*\*Saved Place*\MamacOS"
+move "C:\Users\*username*\*Saved Place*\MamacOS\system\Menu.bat" - "C:\Users\*username*\*Saved Place*\MamacOS"
 echo if the old one is still appearing, Download manually from https://github.com/gtaha23/MamacOS-Batch/blob/main/Ders.bat .
 pause 
 cls
